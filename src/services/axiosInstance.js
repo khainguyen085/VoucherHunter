77a41e92da2a4bb10414761fcb8206266f351a27
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-05-27 20:24:06 +0700

    login

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://localhost:8000",
+  baseURL: "https://team2be.herokuapp.com/",
 });
 
 export const setAuthToken = (token) => {
