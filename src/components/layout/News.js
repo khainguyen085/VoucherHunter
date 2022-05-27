3ce378f05ea82c9eb32581a8ff4be174d704e62f
phongbmt511 <phongbmt178@gmail.com>
2022-05-27 19:59:56 +0700

    Merge code

--- src/components/layout/News.js
+++ src/components/layout/News.js
@@ -2,21 +2,17 @@ import React from "react";
 import { Row, Card, Col } from "react-bootstrap";
 const News = () => {
   return (
-    <div className="page-wrapper">
-      <Row xs={1} md={4} className="g-4">
+    <div>
+      <Row xs={1} md={4} className="g-4" style={{ padding: "10px" }}>
         {Array.from({ length: 8 }).map((_, idx) => (
           <Col>
             <Card>
-              <Card.Img
-                variant="top"
-                src="https://cdn1.hoanghamobile.com/tin-tuc/wp-content/uploads/2020/03/nhung-smartphone-chup-anh-dep-1.jpg"
-              />
+              <Card.Img variant="top" src="https://cdn1.hoanghamobile.com/tin-tuc/wp-content/uploads/2020/03/nhung-smartphone-chup-anh-dep-1.jpg" />
               <Card.Body>
                 <Card.Title>Card title</Card.Title>
                 <Card.Text>
                   This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  lead-in to additional content. This content is a little bit longer.
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -24,6 +20,6 @@ const News = () => {
         ))}
       </Row>
     </div>
-  );
-};
+  )
+}
 export default News;
